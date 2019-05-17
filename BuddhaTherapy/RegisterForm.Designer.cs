25613516baccda22bd4034ed6e512a9e77bd33d9
tiagossa1 <tssa.2000@tuta.io>
2019-05-17 20:57:42 +0100

    Initial work.

---
+++ BuddhaTherapy/RegisterForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace BuddhaTherapy
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterForm));
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.btnShowPassword = new System.Windows.Forms.Button();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.comboGender = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(203, 112);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Guardar e Registar!";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(83, 6);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(238, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 9);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(38, 13);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Nome:";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(12, 36);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(58, 13);
+            this.labelUsername.TabIndex = 3;
+            this.labelUsername.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(83, 32);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(238, 20);
+            this.txtUsername.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(83, 59);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(210, 20);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(12, 62);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(56, 13);
+            this.labelPassword.TabIndex = 7;
+            this.labelPassword.Text = "Password:";
+            // 
+            // btnShowPassword
+            // 
+            this.btnShowPassword.BackgroundImage = global::BuddhaTherapy.Properties.Resources.eye;
+            this.btnShowPassword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnShowPassword.Location = new System.Drawing.Point(299, 59);
+            this.btnShowPassword.Name = "btnShowPassword";
+            this.btnShowPassword.Size = new System.Drawing.Size(22, 20);
+            this.btnShowPassword.TabIndex = 8;
+            this.btnShowPassword.UseVisualStyleBackColor = true;
+            this.btnShowPassword.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // labelGender
+            // 
+            this.labelGender.AutoSize = true;
+            this.labelGender.Location = new System.Drawing.Point(12, 88);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(45, 13);
+            this.labelGender.TabIndex = 9;
+            this.labelGender.Text = "Género:";
+            // 
+            // comboGender
+            // 
+            this.comboGender.FormattingEnabled = true;
+            this.comboGender.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino"});
+            this.comboGender.Location = new System.Drawing.Point(83, 85);
+            this.comboGender.Name = "comboGender";
+            this.comboGender.Size = new System.Drawing.Size(238, 21);
+            this.comboGender.TabIndex = 10;
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 149);
+            this.Controls.Add(this.comboGender);
+            this.Controls.Add(this.labelGender);
+            this.Controls.Add(this.btnShowPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "RegisterForm";
+            this.Text = "Registo da Terapeuta";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RegisterForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button btnShowPassword;
+        private System.Windows.Forms.Label labelGender;
+        private System.Windows.Forms.ComboBox comboGender;
+    }
+}
